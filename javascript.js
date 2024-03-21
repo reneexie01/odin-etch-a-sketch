90356a376c7d7790e748ca1d1e102c2d654b3845
Renee <156127959+reneexie01@users.noreply.github.com>
2024-03-22 06:25:23 +1100

    Set up 16x16 grid

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
-console.log('test');
+const container = document.querySelector('#container');
+
+function generateGrid(num){
+    /* columns */
+    for (let i = 0; i < num; i++) {
+        const  column = document.createElement("div");
+        column.setAttribute('id', `column-${i}`);
+        column.setAttribute('class', 'columns');
+        for (let j = 0; j < num; j++) {
+            const row = document.createElement('div');
+            row.setAttribute('id', `row-${j}`);
+            row.setAttribute('class', 'rows');
+            column.appendChild(row);
+        }
+        container.appendChild(column);
+    }
+}
+
+generateGrid(16);
